c69313a1880b37944faf2bdc23fb63f4e1fdb56c
Qwabeee <98186805+Qwabeee@users.noreply.github.com>
2025-05-28 19:44:07 +0200

    Inserted components in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Team from "./components/Team";
+import BackToTop from "./components/BackToTop";
 
 const App = () => {
-  return <>
-   
-  </>
-}
+  return (
+    <div className="App">
 
-export default App
+      <section id="hero">
+        <Hero />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="team">
+        <Team />
+      </section>
+
+      <BackToTop />
+    </div>
+  );
+};
+
+export default App;
